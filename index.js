7b10024e174cb4704aaf1932e79092f4a10b357e
Zaid <zaidjaved182@gmail.com>
2024-05-01 23:10:45 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,257 @@
+const canvas = document.getElementById('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillStyle = '#000';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.7;
+
+class Sprite {
+    constructor({ position, velocity, color = 'red', offset }) {
+        this.lastKey;
+        this.isAttacking;
+
+        this.width = 50;
+        this.height = 150;
+        this.health = 100;
+
+        this.position = position;
+        this.velocity = velocity;
+        this.color = color;
+
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50
+        }
+    }
+
+    draw() {
+        c.fillStyle = this.color;
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        //attack box
+        c.fillStyle = 'green';
+        if (this.isAttacking) {
+            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
+        }
+    }
+
+    update() {
+        this.draw();
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+        this.attackBox.position.y = this.position.y;
+
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        }
+        else {
+            this.velocity.y += gravity;
+        }
+    }
+
+    attack() {
+        this.isAttacking = true;
+        setTimeout(() => {
+            this.isAttacking = false;
+        }, 100);
+    }
+}
+
+const player = new Sprite({
+    position: { x: 0, y: 0 },
+    velocity: { x: 0, y: 0 },
+    offset: { x: 0, y: 0 }
+});
+
+const enemy = new Sprite({
+    position: { x: 400, y: 180 },
+    velocity: { x: 0, y: 0 },
+    offset: { x: -50, y: 0 },
+    color: 'blue'
+});
+
+const keys = {
+    a: { pressed: false },
+    d: { pressed: false },
+    w: { pressed: false },
+    ArrowRight: { pressed: false },
+    ArrowLeft: { pressed: false },
+    ArrowUp: { pressed: false }
+}
+
+let timer = 10;
+let timerId;
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+}
+
+function showWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId);
+    document.getElementById("result").style.display = "flex";
+
+    if (player.health === enemy.health) {
+        document.getElementById("result").innerHTML = "TIE";
+    }
+    else if (player.health > enemy.health) {
+        document.getElementById("result").innerHTML = "P1 WINS";
+    }
+    else {
+        document.getElementById("result").innerHTML = "P2 WINS";
+    }
+}
+
+function decreaseTime() {
+    timerId = setTimeout(decreaseTime, 1000);
+    if (timer > 0) {
+        timer--;
+        document.getElementById("timer").innerHTML = timer;
+    }
+
+    if (timer === 0) {
+        showWinner({ player, enemy, timerId });
+    }
+}
+
+
+function animate() {
+    window.requestAnimationFrame(animate);
+
+    c.fillStyle = '#000';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    player.update();
+    enemy.update();
+
+    player.velocity.x = 0;
+    enemy.velocity.x = 0;
+
+    //player movement
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -5;
+    }
+    else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 5;
+    }
+
+    //enemy movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -5;
+    }
+    else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 5;
+    }
+
+    //detect collision
+    if (
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy
+        })
+        && player.isAttacking
+    ) {
+        player.isAttacking = false;
+        enemy.health -= 20;
+        document.getElementById("enemyHealth").style.width = `${enemy.health}%`;
+    }
+
+    if (
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player
+        })
+        && enemy.isAttacking
+    ) {
+        enemy.isAttacking = false;
+        player.health -= 20;
+        document.getElementById("playerHealth").style.width = `${player.health}%`;
+    }
+
+    //end game for health less than 0
+    if (enemy.health <= 0 || player.health <= 0) {
+        showWinner({ player, enemy, timerId });
+    }
+}
+
+animate();
+decreaseTime();
+
+window.addEventListener('keydown', (e) => {
+    // e.preventDefault();
+
+    switch (e.key) {
+        case 'd':
+            keys.d.pressed = true;
+            player.lastKey = 'd';
+            break;
+
+        case 'a':
+            keys.a.pressed = true;
+            player.lastKey = 'a';
+            break;
+
+        case 'w':
+            player.velocity.y = -20;
+            break;
+
+        case ' ':
+            player.attack();
+            break;
+
+        //enemy
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true;
+            enemy.lastKey = 'ArrowRight';
+            break;
+
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
+            enemy.lastKey = 'ArrowLeft';
+            break;
+
+        case 'ArrowUp':
+            enemy.velocity.y = -20;
+            break;
+
+        case 'ArrowDown':
+            enemy.attack();
+            break;
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'd':
+            keys.d.pressed = false;
+            break;
+
+        case 'a':
+            keys.a.pressed = false;
+            break;
+
+        //enemy
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false;
+            break;
+
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false;
+            break;
+    }
+})
